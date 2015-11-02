5dc0fabe7fa37898fa65d6a982bc3f80034b5871
wangbintao1992 <541437863@qq.com>
2015-11-02 18:23:58 +0800

    tinymca

--- cube/WebRoot/blog/js/admin.js
+++ cube/WebRoot/blog/js/admin.js
@@ -1,4 +1,4 @@
-var admin = angular.module("admin", ['ui.router','ui.bootstrap','ngFileUpload','ngDialog','ui.grid','ui.grid.selection']);
+var admin = angular.module("admin", ['ui.router','ui.bootstrap','ngFileUpload','ngDialog','ui.grid','ui.grid.selection','ui.tinymce']);
 
 admin.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.when("", "/admin");
@@ -13,7 +13,16 @@ admin.config(function($stateProvider, $urlRouterProvider){
 admin.controller('menuCtrl', function ($scope, $log) {
 	$scope.menu = [{title:'test',child:[{title:'ch1',url:'articleMange'},{title:'ch12',url:'articleMange'},{title:'ch32',url:'articleMange'}]},{title:'test2',child:[{title:'ch2',url:'articleMange'},{title:'ch22',url:'articleMange'},{title:'ch32',url:'articleMange'}]}];
 });
-admin.controller('gridCtrl', ['$scope', '$http',function ($scope, $http,ngDialog) {
+
+admin.controller('addCtrl', function ($scope, $http,ngDialog,$sce) {
+    var ctrl = this;
+    this.updateHtml = function() {
+        ctrl.tinymceHtml = $sce.trustAsHtml(ctrl.tinymce);
+    };
+})
+
+admin.controller('gridCtrl', function ($scope, $http,ngDialog) {
+
     $scope.adds = function(){
         var s = $scope.gridApi.selection.getSelectedGridRows();
         var e = s[0].entity;
@@ -21,8 +30,9 @@ admin.controller('gridCtrl', ['$scope', '$http',function ($scope, $http,ngDialog
     }
     
     $scope.add = function(){
-    	ngDialog.open({
-            template: '<p>tufvutfvuuk</p>',
+        ngDialog.open({
+            template: 'addForm.htm',
+            controller: 'addCtrl',
             className: 'ngdialog-theme-default'
         });
     }
@@ -42,56 +52,6 @@ admin.controller('gridCtrl', ['$scope', '$http',function ($scope, $http,ngDialog
             "company": "Enersol"
         },
             {
-                "name": "Claudine Neal",
-                "gender": "female",
-                "company": "Sealoud"
-            },
-            {
-                "name": "Beryl Rice",
-                "gender": "female",
-                "company": "Velity"
-            },
-            {
-                "name": "Wilder Gonzales",
-                "gender": "male",
-                "company": "Geekko"
-            },
-            {
-                "name": "Georgina Schultz",
-                "gender": "female",
-                "company": "Suretech"
-            },
-            {
-                "name": "Carroll Buchanan",
-                "gender": "male",
-                "company": "Ecosys"
-            },
-            {
-                "name": "Valarie Atkinson",
-                "gender": "female",
-                "company": "Hopeli"
-            },
-            {
-                "name": "Schroeder Mathews",
-                "gender": "male",
-                "company": "Polarium"
-            },
-            {
-                "name": "Lynda Mendoza",
-                "gender": "female",
-                "company": "Dogspa"
-            },
-            {
-                "name": "Sarah Massey",
-                "gender": "female",
-                "company": "Bisba"
-            },
-            {
-                "name": "Robles Boyle",
-                "gender": "male",
-                "company": "Comtract"
-            },
-            {
                 "name": "Evans Hickman",
                 "gender": "male",
                 "company": "Parleynet"
@@ -103,4 +63,4 @@ admin.controller('gridCtrl', ['$scope', '$http',function ($scope, $http,ngDialog
             }
         ]
     };
-}]);
+});
